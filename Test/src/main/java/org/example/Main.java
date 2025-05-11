2cc6d0a95c86c70e53728f9b4c0eac313611314c
XingYa <2814417740@qq.com>
2025-05-11 17:22:41 +0800

    打印小贾

--- Test/src/main/java/org/example/Main.java
+++ Test/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello github!");
+
+
+        boy boy=new boy();
+        System.out.println(boy.name);
+
+
+
     }
 }
