33584192674c71cd33c969b06e040229846f37fa
EatFan <eatfan0921@163.com>
2025-05-14 17:38:32 +0800

    添加：Login模块

---
+++ Login/src/main/java/cn/fan/login/LoginApplication.java
@@ -0,0 +1,12 @@
+package cn.fan.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
